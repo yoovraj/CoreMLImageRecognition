c41fb588f452d80046ccf0c5796514a8dc6397a7
yoovraj <yashinde@gmail.com>
2018-04-21 14:42:10 +0900

    Initial Commit

---
+++ CoreMLImageRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLImageRecognition
+//
+//  Created by yoovraj shinde on 2018/04/21.
+//  Copyright © 2018 ___YOOVRAJSHINDE___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
